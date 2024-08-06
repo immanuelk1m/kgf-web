bcfce56a987c3926ce312d2c03a57822fe4e094b
immanuelk1m <kse0119@naver.com>
2024-08-06 10:49:33 +0900

    update google ana

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,15 @@ export default function RootLayout({
       <head>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7656508177587264"
      crossOrigin="anonymous"></script>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WY310Z2L55"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WY310Z2L55');
+        </script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
