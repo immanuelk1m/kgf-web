2be08c0370c37bac8bf939ecd61912919f507f9c
immanuelk1m <kse0119@naver.com>
2025-03-06 23:16:19 +0900

    strong

--- src/components/component/adsensehd.tsx
+++ src/components/component/adsensehd.tsx
@@ -5,13 +5,20 @@ import { Adsense } from '@ctrl/react-adsense';
 
 function AdsenseOnheader() {
     return (
-        <Adsense               
-            client="ca-pub-7656508177587264"
-            slot="1773657493"
-            format="auto"
-            responsive="true"
-            style={{ display: 'block', margin: '0 auto' }}
-        />
+        <div style={{
+            width: '100%', 
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center'
+        }}>
+            <Adsense               
+                client="ca-pub-7656508177587264"
+                slot="1773657493"
+                format="auto"
+                responsive="true"
+                style={{ display: 'block' }}
+            />
+        </div>
     )
 }
 
