7af96e5a299c6b86c68dea3228d0c829233eb84c
immanuelk1m <kse0119@naver.com>
2025-03-06 23:23:30 +0900

    frr

--- src/components/component/adsensehd.tsx
+++ src/components/component/adsensehd.tsx
@@ -5,20 +5,13 @@ import { Adsense } from '@ctrl/react-adsense';
 
 function AdsenseOnheader() {
     return (
-        <div style={{
-            width: '100%', 
-            display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center'
-        }}>
-            <Adsense               
-                client="ca-pub-7656508177587264"
-                slot="1773657493"
-                format="auto"
-                responsive="true"
-                style={{ display: 'block' }}
-            />
-        </div>
+        <Adsense               
+            client="ca-pub-7656508177587264"
+            slot="1773657493"
+            format="auto"
+            responsive="true"
+            style={{ display: 'block' }}
+        />
     )
 }
 
--- src/components/component/header.tsx
+++ src/components/component/header.tsx
@@ -2,16 +2,12 @@
 import React from 'react';
 import AdsenseOnheader from '@/components/component/adsensehd';
 
-const Ad = () => (
-  <div className="bg-[#F2FBF7] h-full flex justify-center items-center">
-    <AdsenseOnheader/>
-  </div>
-);
-
 const Header: React.FC = () => {
   return (
     <div style={{ height: 'auto', minHeight: '300px' }}>
-      <Ad />
+      <div className="bg-[#F2FBF7] h-full" style={{ textAlign: 'center' }}>
+        <AdsenseOnheader />
+      </div>
     </div>
   );
 };
