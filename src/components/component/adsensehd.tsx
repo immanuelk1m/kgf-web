20dce1f6afbb5130a1f0d7ae834ee7b12dfc076d
immanuelk1m <kse0119@naver.com>
2025-03-06 23:13:47 +0900

    fix again

--- src/components/component/adsensehd.tsx
+++ src/components/component/adsensehd.tsx
@@ -5,15 +5,13 @@ import { Adsense } from '@ctrl/react-adsense';
 
 function AdsenseOnheader() {
     return (
-        <div className="flex justify-center w-full">
-            <Adsense               
-                client="ca-pub-7656508177587264"
-                slot="1773657493"
-                format="auto"
-                responsive="true"
-                style={{ display: 'block' }}
-            />
-        </div>
+        <Adsense               
+            client="ca-pub-7656508177587264"
+            slot="1773657493"
+            format="auto"
+            responsive="true"
+            style={{ display: 'block', margin: '0 auto' }}
+        />
     )
 }
 
