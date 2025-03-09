16b833e9b828af9e664c971a302e9f96751a4a3a
immanuelk1m <kse0119@naver.com>
2025-03-09 21:51:50 +0900

    h2 to h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,7 +124,7 @@ function App() {
           {/* 코스피 이동 평균 차트 (Kospiema) */}
           <div className="w-full p-4">
             <div className="mb-4">
-              <h2 className="text-lg font-semibold text-gray-800">시장 모멘텀 (Market Momentum)</h2>
+              <h1 className="text-lg font-semibold text-gray-800">시장 모멘텀 (Market Momentum)</h1>
               <h3 className="text-lg font-semibold text-gray-800">KOSPI와 125일 이동평균선</h3>
             </div>
             
@@ -144,53 +144,53 @@ function App() {
           </div>
 
           {/* 시장 변동성 차트 (Vixema) */}
-<div className="w-full p-4">
-  {/* 헤더 영역 */}
-  <div className="mb-4">
-    <h2 className="text-lg font-semibold text-gray-800">시장 변동성 (Market Volatility)</h2>
-    <h3 className="text-lg font-semibold text-gray-800">VIX와 VIX 50일 이동평균선</h3>
-  </div>
+          <div className="w-full p-4">
+            {/* 헤더 영역 */}
+            <div className="mb-4">
+              <h1 className="text-lg font-semibold text-gray-800">시장 변동성 (Market Volatility)</h1>
+              <h3 className="text-lg font-semibold text-gray-800">VIX와 VIX 50일 이동평균선</h3>
+            </div>
 
-  {/* 차트와 설명 영역 */}
-  <div className="flex flex-col md:flex-row">
-    <div className="w-full md:w-3/5 border-2 border-gray-200 p-6 bg-gray-50 rounded-lg shadow transition hover:shadow-lg">
-      <Vixema />
-    </div>
-    <div className="w-full md:w-2/5 p-4">
-      <div className="border-2 border-gray-200 p-4 bg-white rounded-lg shadow text-sm">
-        <p className="mt-2 text-gray-600">
-          가장 잘 알려진 시장 심리 지표는 CBOE 변동성 지수(VIX)입니다. VIX는 코스피 옵션의 30일간 예상 변동성을 측정하며, 시장이 상승할 때 하락하고, 시장이 급락할 때 급등하는 경향이 있습니다. 장기적으로 보면 강세장에서는 낮고, 약세장에서는 높아지는 특징이 있습니다. 공포 & 탐욕 지수는 변동성이 증가할 때 ‘공포’ 신호로 봅니다.
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
+            {/* 차트와 설명 영역 */}
+            <div className="flex flex-col md:flex-row">
+              <div className="w-full md:w-3/5 border-2 border-gray-200 p-6 bg-gray-50 rounded-lg shadow transition hover:shadow-lg">
+                <Vixema />
+              </div>
+              <div className="w-full md:w-2/5 p-4">
+                <div className="border-2 border-gray-200 p-4 bg-white rounded-lg shadow text-sm">
+                  <p className="mt-2 text-gray-600">
+                    가장 잘 알려진 시장 심리 지표는 CBOE 변동성 지수(VIX)입니다. VIX는 코스피 옵션의 30일간 예상 변동성을 측정하며, 시장이 상승할 때 하락하고, 시장이 급락할 때 급등하는 경향이 있습니다. 장기적으로 보면 강세장에서는 낮고, 약세장에서는 높아지는 특징이 있습니다. 공포 & 탐욕 지수는 변동성이 증가할 때 ‘공포’ 신호로 봅니다.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
 
-{/* 맥클렐런 거래량 지수 (Mccl) */}
-<div className="w-full p-4">
-  <div className="mb-4">
-    <h2 className="text-lg font-semibold text-gray-800">주가 폭 (Stock Price Breadth)</h2>
-    <h3 className="text-lg font-semibold text-gray-800">McClellan Volume Summation Index</h3>
-  </div>
+          {/* 맥클렐런 거래량 지수 (Mccl) */}
+          <div className="w-full p-4">
+            <div className="mb-4">
+              <h1 className="text-lg font-semibold text-gray-800">주가 폭 (Stock Price Breadth)</h1>
+              <h3 className="text-lg font-semibold text-gray-800">McClellan Volume Summation Index</h3>
+            </div>
 
-  <div className="flex flex-col md:flex-row">
-    <div className="w-full md:w-3/5 border-2 border-gray-200 p-6 bg-gray-50 rounded-lg shadow transition hover:shadow-lg">
-      <Mccl />
-    </div>
-    <div className="w-full md:w-2/5 p-4">
-      <div className="border-2 border-gray-200 p-4 bg-white rounded-lg shadow text-sm">
-        <p className="mt-2 text-gray-600">
-          맥클렐런 거래량 합산 지수는 상승하는 주식의 거래량과 하락하는 주식의 거래량을 비교하는 지표입니다. 이 지표가 낮거나 음수이면 약세 신호로 해석됩니다. 공포 & 탐욕 지수는 거래량 감소를 ‘공포’ 신호로 봅니다.
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
+            <div className="flex flex-col md:flex-row">
+              <div className="w-full md:w-3/5 border-2 border-gray-200 p-6 bg-gray-50 rounded-lg shadow transition hover:shadow-lg">
+                <Mccl />
+              </div>
+              <div className="w-full md:w-2/5 p-4">
+                <div className="border-2 border-gray-200 p-4 bg-white rounded-lg shadow text-sm">
+                  <p className="mt-2 text-gray-600">
+                    맥클렐런 거래량 합산 지수는 상승하는 주식의 거래량과 하락하는 주식의 거래량을 비교하는 지표입니다. 이 지표가 낮거나 음수이면 약세 신호로 해석됩니다. 공포 & 탐욕 지수는 거래량 감소를 ‘공포’ 신호로 봅니다.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
 
 {/* 풋 & 콜 옵션 차트 (Pcema) */}
 <div className="w-full p-4">
   <div className="mb-4">
-    <h2 className="text-lg font-semibold text-gray-800">풋 & 콜 옵션 (Put & Call Options)</h2>
+    <h1 className="text-lg font-semibold text-gray-800">풋 & 콜 옵션 (Put & Call Options)</h1>
     <h3 className="text-lg font-semibold text-gray-800">5일 이동평균선</h3>
   </div>
 
@@ -211,7 +211,7 @@ function App() {
 {/* 안전 자산 선호도 차트 (Safeb) */}
 <div className="w-full p-4">
   <div className="mb-4">
-    <h2 className="text-lg font-semibold text-gray-800">안전 자산 선호도 (Safe Haven Demand)</h2>
+    <h1 className="text-lg font-semibold text-gray-800">안전 자산 선호도 (Safe Haven Demand)</h1>
     <h3 className="text-lg font-semibold text-gray-800">채권 수익률과 주식 수익률의 차이</h3>
   </div>
 
@@ -232,7 +232,7 @@ function App() {
 {/* 정크본드 수요 차트 (Junks) */}
 <div className="w-full p-4">
   <div className="mb-4">
-    <h2 className="text-lg font-semibold text-gray-800">정크본드 수요 (Junk Bond Demand)</h2>
+    <h1 className="text-lg font-semibold text-gray-800">정크본드 수요 (Junk Bond Demand)</h1>
     <h3 className="text-lg font-semibold text-gray-800">정크본드와 투자등급 채권 간의 수익률 차이</h3>
   </div>
 
